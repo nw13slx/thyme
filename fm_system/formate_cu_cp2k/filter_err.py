f370b8f8793e23b06d09649f1b4de82471113e23
nw13slx <nw13mifaso@gmail.com>
2020-10-14 01:28:22 -0400

    update

--- fm_system/formate_cu_cp2k/filter_err.py
+++ fm_system/formate_cu_cp2k/filter_err.py
@@ -7,19 +7,35 @@ import numpy as np
 
 from fmeee.trajectories import Trajectories
 from fmeee.filters.error import sort_by_force
+from fmeee.routines.write.xyz import write_single_xyz
 
 
 trjs = Trajectories.from_file("trjs.pickle")
-new_trjs = Trajectories()
-alldata = new_trjs.alldata
+train = Trajectories()
+traindata = train.alldata
+test = Trajectories()
+testdata = test.alldata
 
 for i, trj in enumerate(trjs.alldata.values()):
     sorted_id = sort_by_force(trj, 'pred', 'O')
     if i==0:
-        new_trj = trj.skim(sorted_id[:-10:3])
+        new_trj = trj.skim(sorted_id[:-20:3])
+        new_trj2 = trj.skim(sorted_id[1:-20:3])
     else:
-        new_trj = trj.skim(sorted_id[:-20:50])
-    alldata[trj.name] = new_trj
-    print(new_trjs.nframes)
-new_trjs.save_padded_matrices(f"{new_trjs.nframes}frames_by_df.npz")
+        new_trj = trj.skim(sorted_id[:-50:30])
+        new_trj2 = trj.skim(sorted_id[1:-50:30])
+    traindata[trj.name] = new_trj
+    testdata[trj.name] = new_trj2
 
+join_train = train.to_padded_trajectory()
+join_train.shuffle()
+join_test = test.to_padded_trajectory()
+join_test.shuffle()
+
+join_train.add_trj(join_test)
+
+join_train.save(f"{train.nframes}frames_by_df.npz")
+write_single_xyz(join_train, f"{train.nframes}frames_by_df")
+
+# multiple_plots_e(trjs, pred_label='pe')
+# multiple_plots(trjs, pred_label='pred')
