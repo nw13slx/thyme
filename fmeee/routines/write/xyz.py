441408b1c4f7e9e5923e6a4c59163fd02b5e1e01
nw13slx <nw13mifaso@gmail.com>
2020-10-13 22:21:42 -0400

    add xyz writing

---
+++ fmeee/routines/write/xyz.py
@@ -0,0 +1,18 @@
+import logging
+import numpy as np
+
+from ase.atoms import Atoms
+from ase.io.extxyz import write_xyz
+
+def write_single_xyz(trj, prefix):
+    """
+    """
+    for i in range(trj.nframes):
+        atoms = Atoms(trj.species, trj.positions[i], cell=trj.cells[i], pbc=True)
+        write_xyz(f"{prefix}.xyz", atoms, append=True)
+
+def write_xyzs(trjs, prefix=""):
+    """
+    """
+    for trj in trjs.alldata.values():
+        write_single_xyz(trj, f"{prefix}{trj.name}")
