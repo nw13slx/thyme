4310d94ad5194a61bd02ce8bf7f6ceb2e0ca5e83
nw13slx <nw13mifaso@gmail.com>
2020-10-27 17:51:46 -0400

    fix bug in filter

--- fm_system/formate_au_vasp/collect.py
+++ fm_system/formate_au_vasp/collect.py
@@ -27,6 +27,7 @@ def e_filter(trj):
         atoms = Atoms(species, xyz, cell=c, pbc=True)
         dist_mat = atoms.get_all_distances(mic=True)
         not_Au = [i for i in range(dist_mat.shape[0]) if species[i] != 'Au']
+        accept = True
         for ind in range(dist_mat.shape[0]):
 
             neigh = np.argmin(np.hstack([dist_mat[ind, :ind], dist_mat[ind, ind+1:]]))
@@ -34,10 +35,12 @@ def e_filter(trj):
                 neigh += 1
             mindist = dist_mat[ind, neigh]
 
-            if ind in not_Au and mindist > 2.0:
-                logging.info(f"skip frame for isolated CHO atom {mindist} {ind}"
-                             f" {species[ind]} {neigh} {species[neigh]}")
-            elif ind not in not_Au:
+            if ind in not_Au:
+                if mindist > 2.0:
+                    logging.info(f"skip frame for isolated CHO atom {mindist} {ind}"
+                                 f" {species[ind]} {neigh} {species[neigh]}")
+                    accept = False
+            else:
                 # if mindist > 3.0:
                 #     logging.info(f"skip frame for isolated Au atom {mindist} {ind} {species[ind]} {neigh} {species[neigh]}")
                 Au = [i for i in range(dist_mat.shape[0]) if species[i] == 'Au' and i!=ind]
@@ -47,7 +50,9 @@ def e_filter(trj):
                     print(ind, dist_mat[ind, ind-3:np.min([ind+3, dist_mat.shape[0]])])
                     logging.info(f"skip frame for isolated Au from other Au {minAu} {ind}"
                                  f" {species[ind]} {neigh} {species[neigh]}")
-                    accept_frame += [i]
+                    accept = False
+        if accept:
+            accept_frame += [i]
 
     return np.array(accept_frame, dtype=int)
 
