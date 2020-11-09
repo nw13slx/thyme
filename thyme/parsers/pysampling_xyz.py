9d500320ee01e0eefdcbf84bc3ea1d33c676be57
Lixin Sun <nw13mifaso@gmail.com>
2020-11-09 14:20:20 -0600

    move the labeling to scripts

--- thyme/parsers/pysampling_xyz.py
+++ thyme/parsers/pysampling_xyz.py
@@ -24,33 +24,6 @@ def pack_folder_trj(folder, data_filter, include_xyz=True):
     for filename in glob(f"{folder}/*_*.xyz"):
         if isfile(filename):
             trj = extxyz_to_padded_trj(filename)
-            labeling(trj)
             if trj.nframes > 0:
                 trjs.add_trj(trj, filename)
     return trjs
-
-def labeling(trj):
-
-    species = trj.symbols[-1]
-    atoms = Atoms(species, trj.positions[-1], cell=trj.cells[-1], pbc=True)
-
-    Hid = np.array([i for i, s in enumerate(species) if s == 'H'])
-    Cid = np.array([i for i, s in enumerate(species) if s == 'C'])
-    Oid = np.array([i for i, s in enumerate(species) if s == 'O'])
-    Cuid = np.array([i for i, s in enumerate(species) if s == 'Cu'])
-
-    dist_mat = atoms.get_all_distances(mic=True)
-    dist_CH = np.max((dist_mat[Hid].T)[Cid])
-
-    dist_OCu = np.max(np.min((dist_mat[Oid].T)[Cuid], axis=0))
-    dist_HCu = np.min((dist_mat[Hid].T)[Cuid])
-
-    label = -1
-    if dist_CH > 2.5 and dist_OCu > 3 and dist_HCu < 1.8:
-        label = 1
-    elif dist_CH <= 1.2 and dist_OCu < 2.2 and dist_HCu > 3:
-        label = 0
-
-    trj.labels = np.zeros(trj.nframes) + label
-    trj.per_frame_attrs += ['labels']
-    logging.info(f"label as {label}")
--- thyme/routines/dist_plots/energy.py
+++ thyme/routines/dist_plots/energy.py
@@ -6,12 +6,16 @@ import matplotlib.pyplot as plt
 plt.switch_backend('agg')
 
 
-def multiple_plots(trajectories, prefix=""):
-
+def multiple_plots(trajectories, label='energies', prefix="", xlabel="Energy (eV)"):
     for i, trj in enumerate(trajectories.alldata.values()):
-        single_plot(trj, prefix)
+        single_plot(trj, label, prefix, xlabel)
 
-
-def single_plot(trj, prefix=""):
+def single_plot_energy(trj, prefix=""):
     base_line_hist(trj.energies, "Energy (eV)",
                    f"{prefix}{trj.name}_energy_dist")
+
+def single_plot(trj, label='energies', prefix="", xlabel="Energy (eV)"):
+    item = getattr(trj, label, None)
+    if item is not None:
+        base_line_hist(item, xlabel,
+                       f"{prefix}{trj.name}_{label}_dist")
