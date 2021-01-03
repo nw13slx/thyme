ed316062ab3a5d331e8c74685292dc5e88f84711
nw13slx <nw13mifaso@gmail.com>
2021-01-03 17:57:58 -0500

    add new filter

--- scripts/formate_au_vasp/collect_merge.py
+++ scripts/formate_au_vasp/collect_merge.py
@@ -1,5 +1,4 @@
 from thyme.filters.distance import e_filter
-from thyme.filters.energy import sort_e
 from thyme.routines.dist_plots.energy import multiple_plots as multiple_plots_e
 from thyme.parsers.extxyz import write_trjs
 from thyme.parsers.vasp import pack_folder_trj, get_childfolders
@@ -8,7 +7,7 @@ from thyme.trajectories import Trajectories
 from ase.atoms import Atoms
 import numpy as np
 import logging
-logging.basicConfig(filename=f'collect.log', filemode='w',
+logging.basicConfig(filename=f'collect_merge.log', filemode='w',
                     level=logging.INFO, format="%(message)s")
 logging.getLogger().addHandler(logging.StreamHandler())
 
@@ -16,18 +15,13 @@ logging.getLogger().addHandler(logging.StreamHandler())
 def main():
 
     # folders = get_childfolders("./")
-    # # folders = [f"{i+1}" for i in range(59)]
+    # folders = [f"{i+1}" for i in range(59)]
     # trjs = parse_merged_folders_trjs(folders, pack_folder_trj,
-    #                           e_filter, "all_data.pickle")
+    #                           e_filter, npz_filename="all_data.pickle", merge_level=1)
     trjs = Trajectories.from_file('all_data.pickle')
-    # merge the one with the same composition
-    trjs = Trajectories.from_padded_trajectory(trjs.to_padded_trajectory())
-    for trj in trjs.alldata.values():
-        accept_id = sort_e(trj)
-        trj.filter_frames(accept_id)
-        write_trjs("all.xyz", trjs)
-
+    trjs = trjs.remerge()
     multiple_plots_e(trjs, prefix='alldata')
+    write_trjs("all.xyz", trjs)
 
 
 if __name__ == '__main__':
--- thyme/filters/distance.py
+++ thyme/filters/distance.py
@@ -4,7 +4,62 @@ import numpy as np
 from ase.atoms import Atoms
 
 
-def e_filter(trj):
+def e_filter(trj, maxE=-200, minE=-1e4):
+    """
+    filter away non-negative energies
+    """
+
+    accept_frame = []
+    species = trj.species
+    energy_id1 = np.where(trj.energies < maxE)[0]
+    energy_id2 = np.where(trj.energies > minE)[0]
+    energy_id = list(set(energy_id1).intersection(set(energy_id2)))
+
+    for i in energy_id:
+        xyz = trj.positions[i]
+        c = trj.cells[i]
+        atoms = Atoms(species, xyz, cell=c, pbc=True)
+        dist_mat = atoms.get_all_distances(mic=True)
+        not_metal = [i for i in range(dist_mat.shape[0]) if species[i] in [
+            'C', 'H', 'O']]
+        accept = True
+        for ind in range(dist_mat.shape[0]):
+
+            neigh = np.argmin(
+                np.hstack([dist_mat[ind, :ind], dist_mat[ind, ind+1:]]))
+            if neigh >= ind:
+                neigh += 1
+            mindist = dist_mat[ind, neigh]
+
+            if ind in not_metal:
+                if mindist > 2.0 and species[ind] != 'O':
+                    logging.info(f"skip frame for isolated CHO atom {mindist} {ind}"
+                                 f" {species[ind]} {neigh} {species[neigh]}")
+                    accept = False
+                elif mindist > 3.0 and species[ind] == 'O':
+                    logging.info(f"skip frame for isolated CHO atom {mindist} {ind}"
+                                 f" {species[ind]} {neigh} {species[neigh]}")
+                    accept = False
+            else:
+                # if mindist > 3.0:
+                #     logging.info(f"skip frame for isolated metal atom {mindist} {ind} {species[ind]} {neigh} {species[neigh]}")
+                metal = [i for i in range(dist_mat.shape[0]) if species[i] not in
+                         ['C', 'H', 'O'] and i != ind]
+                neigh = metal[np.argmin(dist_mat[ind, metal])]
+                min_metal = dist_mat[ind, neigh]
+                if min_metal > 3.5:
+                    print(ind, dist_mat[ind, ind -
+                                        3:np.min([ind+3, dist_mat.shape[0]])])
+                    logging.info(f"skip frame for isolated metal from other metal"
+                                 f" {min_metal} {ind}"
+                                 f" {species[ind]} {neigh} {species[neigh]}")
+                    accept = False
+        if accept:
+            accept_frame += [i]
+
+    return np.array(accept_frame, dtype=int)
+
+def e_disp_filter(trj):
     """
     filter away non-negative energies
     and configs with isolated metal atoms
