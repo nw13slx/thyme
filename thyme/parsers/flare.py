45993ebd6db5eab6b5da972a054722d5d02aa43f
nw13slx <nw13mifaso@gmail.com>
2021-03-26 14:05:56 -0400

    add traj converter

--- thyme/parsers/flare.py
+++ thyme/parsers/flare.py
@@ -9,6 +9,7 @@ from flare.struc import Structure
 
 from thyme.parsers.monty import read_pattern, read_table_pattern
 from thyme.trajectory import PaddedTrajectory, Trajectory
+from thyme.trajectories import Trajectories
 from thyme.routines.folders import find_folders, find_folders_matching
 
 
@@ -40,3 +41,29 @@ def write(filename, trj):
         pickle.dump(structures, fout)
 
     return structures
+
+mapping = dict(energy="energies",
+               forces="forces",
+               nat="natom",
+               species_labels="species",
+               _positions="positions",
+               stress="stresses",
+               )
+def from_file(filename, as_trajectory=True):
+    structure_list = Structure.from_file(filename, as_trajectory=as_trajectory)
+    trjs = Trajectories()
+    for struc in structure_list:
+        d = struc.as_dict()
+        new_dict = { mapping.get(k): d[k] for k in mapping}
+        trjs.add_trj(Trajectory.from_dict(new_dict))
+    trjs.remerge()
+    return trjs
+
+if __name__ == "__main__":
+    import sys
+    trjs = from_file(sys.argv[1])
+    trjs.save("all.pickle")
+    trjs.save("all_padded_mat.npz")
+
+
+
