e688b16b3c3a3031fe21a4f626024a1695521978
nw13slx <nw13mifaso@gmail.com>
2021-11-02 10:43:29 -0400

    add pizza lib files

--- thyme/parsers/lammps.py
+++ thyme/parsers/lammps.py
@@ -5,12 +5,16 @@ from glob import glob
 from os.path import getmtime, isfile
 from os import remove
 
+from thyme import Trajectory
 from thyme.parsers.monty import read_pattern, read_table_pattern
 from thyme.routines.folders import find_folders, find_folders_matching
 from thyme._key import *
+from thyme.parsers.lammps_pizza_log import log as lammps_log
+from thyme.parsers.lammps_pizza_dump import *
 
 fl_num = r"([+-]?\d+.\d+[eE]?[+-]?\d*)"
 sfl_num = r"\s+([+-]?\d+.\d+[eE]?[+-]?\d*)"
+snum = r"\s+([+-]?\d+)"
 nc_fl_num = r"[+-]?\d+.\d+[eE]?[+-]?\d*"
 head_str = """ITEM: TIMESTEP
 {timestep}
@@ -44,8 +48,6 @@ def write(name, trj, color_key="", spe2num={}):
             keys += [color_key]
 
     fout = open(name, "w+")
-
-    print("hello", trj.nframes)
 
     for i in range(trj.nframes):
         frame = trj.get_frame(i)
@@ -83,3 +85,52 @@ def write(name, trj, color_key="", spe2num={}):
     logging.info(f"write {name}")
     fout.close()
     logging.info(f"spe2num {spe2num}")
+
+def from_file(filename):
+
+    data = dump(filename)
+    col_id = data.names["id"]
+    col_type = data.names["type"]
+    x_id = data.names["x"]
+    y_id = data.names["y"]
+    z_id = data.names["z"]
+    if "fx" in data.names:
+        fx_id = data.names["fx"]
+        fy_id = data.names["fy"]
+        fz_id = data.names["fz"]
+
+    trj = Trajectory()
+    for i in range(data.nsnaps):
+        if i%1000 == 0:
+            logging.info(f"{i} / {data.nsnaps}")
+        snap = data.snaps[i]
+        cols = np.vstack(snap.atoms)
+        ids = np.argsort(cols[:, col_id])
+        species = cols[:, col_type][ids]
+        pos = np.hstack((cols[:, x_id].reshape([-1, 1]),
+                         cols[:, y_id].reshape([-1, 1]),
+                         cols[:, z_id].reshape([-1, 1])))
+        lx = snap.xhi-snap.xlo
+        ly = snap.yhi-snap.ylo
+        lz = snap.zhi-snap.zlo
+        d = { CELL: np.diag([lx, ly, lz]).reshape([1, 3, 3]),
+                POSITION: pos[ids].reshape([1, -1, 3]),
+                SPECIES: species,
+                PER_FRAME_ATTRS: [POSITION, CELL],
+                FIXED_ATTRS: [SPECIES, NATOMS],
+                }
+        if "fx" in data.names:
+            force = np.hstack((cols[:, fx_id].reshape([-1, 1]),
+                               cols[:, fy_id].reshape([-1, 1]),
+                               cols[:, fz_id].reshape([-1, 1])))[ids]
+            d.update({FORCE: force.reshape([1, -1, 3])})
+            d[PER_FRAME_ATTRS] += [FORCE]
+        _trj = Trajectory.from_dict(d)
+        trj.add_trj(_trj)
+    return trj
+
+def read_log(filename):
+    l = lammps_log(filename, 0)
+    l.next()
+    data = np.array(l.data)
+    return l.names, data
